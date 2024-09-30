8cb40cd2dcc772e2d76b6c7fe3258855f43fe1f7
RC-Torres <158267813+RC-Torres@users.noreply.github.com>
2024-09-30 09:24:48 +0800

    Add files via upload

---
+++ Laboratory Activity/Laboratory Activity 2 - 30-09-2024/MultiplicationTable.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    // Function for getting the values of the table
+    public static void getTable(int[][] multiplicationTable, int maxNumber) {
+        for (int i = 1; i <= maxNumber; i++) {
+            for (int j = 1; j <= maxNumber; j++) {
+                multiplicationTable[i][j] = i * j;
+            }
+        }
+    }
+    // Function for printing the table
+    public static void printTable(int[][] multiplicationTable, int maxNumber) {
+        for (int i = 1; i <= maxNumber; i++) {
+            for (int j = 1; j <= maxNumber; j++) {
+                int number = multiplicationTable[i][j];
+                System.out.printf("%4d", number); // Prints each number with 4 spaces
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating a scanner to take the max size of the multiplication table
+        Scanner input = new Scanner(System.in);
+
+        // Taking input for the maximum number for the multiplication table
+        System.out.print("Enter the size of the multiplication table: ");
+        int maxNumber = input.nextInt();
+ 
+        // Creating a two-dimensional array for the multiplication table
+        int[][] multiplicationTable = new int[maxNumber + 1][maxNumber + 1];
+
+        // Populating the table by passing to a function
+        getTable(multiplicationTable, maxNumber);
+
+        // Print the multiplication table using nested loops and formatting
+        System.out.println("Multiplication Table:");
+        printTable(multiplicationTable, maxNumber);
+
+        input.close();
+    }
+}
